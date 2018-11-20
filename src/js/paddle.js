cece409157d7ef70aaad467e6f3cf04df48bd0c6
runandrerun <the.asantiagojr@gmail.com>
2018-11-19 20:50:23 -0500

    Added constructors for Vector Positioning and Paddles

---
+++ src/js/paddle.js
@@ -0,0 +1,6 @@
+class Paddle {
+  constructor(w, h) {
+    this.pos = new Position;
+    this.size = new Position(w, h);
+  }
+};
---
+++ src/js/pong.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const canvas = document.getElementById('pong');
+  const context = canvas.getContext('2d');
+
+  // table
+  context.fillStyle = '#ccc';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // ball
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, 10, 10);
+});
