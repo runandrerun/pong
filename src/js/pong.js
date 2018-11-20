2159add5312f66c1348f875d5952eb8a6da192c9
runandrerun <the.asantiagojr@gmail.com>
2018-11-20 10:47:25 -0500

    Animation added

--- src/js/pong.js
+++ src/js/pong.js
@@ -3,15 +3,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('pong');
   const context = canvas.getContext('2d');
 
-  // table
-  context.fillStyle = '#ccc';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  // ball
+  // instantiate ball
   const ball = new Ball;
-  
-  context.fillStyle = '#000';
-  context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
 
-  console.log(ball)
+  let last;
+
+  callBack = (ms) => {
+
+  }
+
+  // movement
+  update = (time) => {
+    ball.pos.x += ball.value.x * time;
+    ball.pos.y += ball.value.y * time
+
+    // ball
+    context.fillStyle = '#000';
+    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+
+    // table
+    context.fillStyle = '#ccc';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
 });
