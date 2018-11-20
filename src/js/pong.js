7f1c520863f10a07fdf4d55e7cccd43d53d8f4b8
runandrerun <the.asantiagojr@gmail.com>
2018-11-20 11:40:05 -0500

    OOP

--- src/js/pong.js
+++ src/js/pong.js
@@ -1,3 +1,32 @@
+class Pong {
+  constructor(canvas) {
+    this._canvas = canvas;
+    this._canvas = canvas.getContext('2d');
+  }
+
+  update = (time) => {
+    ball.pos.x += ball.velocity.x * time;
+    ball.pos.y += ball.velocity.y * time
+
+    // table constraints
+    if (ball.left< 0 || ball.right > canvas.width) {
+      ball.velocity.x = -ball.velocity.x
+    }
+
+    if (ball.top < 0 || ball.bottom > canvas.height) {
+      ball.velocity.y = -ball.velocity.y
+    }
+
+    // ball
+    context.fillStyle = '#000';
+    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+
+    // table
+    context.fillStyle = '#ccc';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
   const canvas = document.getElementById('pong');
@@ -5,9 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // instantiate ball
   const ball = new Ball;
-  ball.pos.x = 50;
-  ball.pos.y = 10;
-  
+  ball.pos.x = 100;
+  ball.pos.y = 50;
+
   ball.velocity.x = 100;
   ball.velocity.y = 100;
 
@@ -23,27 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // ball positioning
-  update = (time) => {
-    ball.pos.x += ball.velocity.x * time;
-    ball.pos.y += ball.velocity.y * time
-
-    // table constraints
-    if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
-      ball.velocity.x = -ball.velocity.x
-    }
-
-    if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
-      ball.velocity.y = -ball.velocity.y
-    }
-
-    // ball
-    context.fillStyle = '#000';
-    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-
-    // table
-    context.fillStyle = '#ccc';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-  }
 
   ballMovement()
 
